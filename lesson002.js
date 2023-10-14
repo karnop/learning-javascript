3e76588969bb1f3ba3ba11b046c66e1aafc73664
karnop <communicate.manav@gmail.com>
2023-10-14 21:45:09 +0530

    added introduction on datatypes

---
+++ lesson002.js
@@ -0,0 +1,24 @@
+// datatypes
+
+"use strict";   // treats all js code as newer version of javascript
+
+//alert(3 + 3);   // only works in web browser not in nodejs
+let name = "Manav";
+let age = 19;
+let isLoggedIn = false;
+let state;      // currently undefined
+state = null    // now null
+
+console.log(typeof(state));  // used to check the data type
+
+// PRIMITIVE DATA TYPES
+// number => 2 ^ 53
+// bigint
+// string => ""
+// boolean => true false
+// null     => standalone value
+// undefined 
+// symbol => used for uniqueness
+
+// NON PRIMITIVE DATA TYPES
+// object 
