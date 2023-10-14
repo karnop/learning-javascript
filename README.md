e1340cd60ffe442f017d4a60758f75d39c07363b
karnop <communicate.manav@gmail.com>
2023-10-14 22:57:04 +0530

    added readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Chapters 
+
+### lesson 001 : 
+> variables : let const var usage
+
+### lesson 002 : 
+> Introduction to DataTypes
+
+### lesson 003 : 
+> // DATATYPE CONVERSION 
+
+### lesson 004 : 
+> String and some useful functions
---
+++ lesson004.js
@@ -0,0 +1,16 @@
+// STRINGS 
+
+let namee = "Manav";
+const repoCount = 7;
+
+console.log(namee + repoCount + "Value"); // not preferred
+console.log(`Hello my name is ${namee} and my repo count is ${repoCount}`); // preferred
+
+console.log(`Size : ${namee.length}`);
+console.log(`Uppercase String : ${namee.toLocaleUpperCase()}`);
+console.log(`char at index 3 : ${namee.charAt(3)}`);
+console.log(`index of a : ${namee.indexOf('a')}`);
+console.log(`last index of a : ${namee.lastIndexOf('a')}`);
+console.log(`index of t : ${namee.indexOf('t')}`);
+
+// partitioning a string
